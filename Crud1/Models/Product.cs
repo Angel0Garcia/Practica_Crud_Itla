cd411186336ad96de49f2186835412c78b632d2e
Ángel García <gabrielg080nac@gmail.com>
2024-10-05 11:58:29 -0400

    Creating models

---
+++ Crud1/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Crud1.Models
+{
+    public class Product
+    {
+        public double pro_id {  get; set; }
+        public string pro_name { get; set; }
+        public double pro_price { get; set; }
+        public string pro_description { get; set; }
+    }
+}
---
+++ Crud1/Models/Receipt.cs
@@ -0,0 +1,11 @@
+﻿namespace Crud1.Models
+{
+    public class Receipt
+    {
+        public double id { get; set; }
+        public string name { get; set; }
+        public double price { get; set; }
+        public string description { get; set; }
+        public double total { get; set; }
+    }
+}
